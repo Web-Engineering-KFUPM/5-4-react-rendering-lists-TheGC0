186990b92a03cadacb9594cd43c0579cbc681196
AliShamah <ali_shamah@outlook.com>
2026-02-25 11:58:04 +0300

    fix: implement checkbox functionality and ensure task state reflects completion

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -7,7 +7,7 @@ export default function TaskItem({ task, onToggle, onDelete }) {
       <label className="taskMain">
         {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
             Also make it reflect task.isDone using checked={task.isDone} */}
-        <input type="checkbox" />
+        <input type="checkbox" checked={task.isDone} onChange={() => onToggle(task.id)} />
 
         {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&)
             Example:
@@ -20,8 +20,10 @@ export default function TaskItem({ task, onToggle, onDelete }) {
               }
       </label>
 
-      {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
-      <button className="ghost" aria-label="Delete task">
+      {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */
+      
+      }
+      <button className="ghost" aria-label="Delete task" onClick={() => onDelete(task.id)}>
         ✕
       </button>
     </li>
